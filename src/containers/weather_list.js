6cd9c9b6e36543c6c51037d25f210e50e985d4da
Boris Civcic <civcicboris93@gmail.com>
2018-08-17 17:31:35 +0200

    google map added to table

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import Chart from '../components/chart'
+import GoogleMap from '../components/google_map'
 
 class WeatherList extends Component {
   renderWeather(cityData) {
@@ -8,10 +9,13 @@ class WeatherList extends Component {
     const temps = cityData.list.map(weather => weather.main.temp)
     const pressure = cityData.list.map(weather => weather.main.pressure)
     const humidities = cityData.list.map(weather => weather.main.humidity)
-
+    //const lon = cityData.city.coord.lon
+    //const lng = cityData.city.coord.lng
+    // sa ES6 mozemo da napisemo ovako:
+    const { lon, lat } = cityData.city.coord // Automacki prvo uzima posle coord, log pa lng promenjivu
     return (
       <tr key={name}>
-        <td>{name}</td>
+        <td><GoogleMap lon={lon} lat={lat}  /></td>
         <td><Chart data={temps} color="orange" units="K" /></td>
         <td><Chart data={pressure} color="blue" units="hPa" /></td>
         <td><Chart data={humidities} color="black" units="%" /></td>
