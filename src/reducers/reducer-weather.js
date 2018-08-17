3dad7dc9c85c6f66c0ddc530c7f5c4a076aeee11
Boris Civcic <civcicboris93@gmail.com>
2018-08-17 13:47:59 +0200

    weather reducer added

---
+++ src/reducers/reducer-weather.js
@@ -0,0 +1,12 @@
+import { FETCH_WEATHER } from '../actions/index'
+
+export default function(state = [], action) {
+  console.log('Active Reducer', action)
+  switch(action.type) {
+  case: FETCH_WEATHER
+    //return state.concat([ action.payload.data ]) //concat isto kao push samo on ne brise stari array nego dodaje u isti nove
+    // u novom ES6 mozemo pisati ovako
+    return [ action.payload.data, ...sate ] // [ city, city, city ]
+  }
+  return state
+}
